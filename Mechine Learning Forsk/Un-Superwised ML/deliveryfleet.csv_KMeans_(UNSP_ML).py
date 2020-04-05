4d488b352712a772e4631e461c55055794069733
Rajesh-sharma92 <sharma90126@gmail.com>
2020-04-05 14:36:39 +0530

    DeliveryFleet.csv_KMeans()

--- Mechine Learning Forsk/Un-Superwised ML/deliveryfleet.csv_KMeans_(UNSP_ML).py
+++ Mechine Learning Forsk/Un-Superwised ML/deliveryfleet.csv_KMeans_(UNSP_ML).py
@@ -14,11 +14,13 @@ Import deliveryfleet.csv file
 Here we need Two driver features: mean distance driven per day (Distance_feature) 
 and the mean percentage of time a driver was >5 mph over the speed limit (speeding_feature).
 
-Perform K-means clustering to distinguish urban drivers and rural drivers.
-Perform K-means clustering again to further distinguish speeding drivers 
+1) Perform K-means clustering to distinguish urban drivers and rural drivers.
+
+2) Perform K-means clustering again to further distinguish speeding drivers 
 from those who follow speed limits, in addition to the rural vs. urban division.
 Label accordingly for the 4 groups.
 """
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -97,8 +99,8 @@ print(kmeans.cluster_centers_[:, 1]) # y
 
 # Central Location of the Cluster == Centroid
 # Centroid = Avg of x coordinate and Avg of y coordinate for lets assume Cluster 1
-plt.scatter(features[pred_cluster == 0, 0], features[pred_cluster == 0, 1], c = 'blue', label = 'Cluster 1')
-plt.scatter(features[pred_cluster == 1, 0], features[pred_cluster == 1, 1], c = 'red', label = 'Cluster 2')
+plt.scatter(features[pred_cluster == 0, 0], features[pred_cluster == 0, 1], c = 'blue', label = 'Rural')
+plt.scatter(features[pred_cluster == 1, 0], features[pred_cluster == 1, 1], c = 'red', label = 'Urban')
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c = 'yellow', label = 'Centroids')
 
 plt.title('Clusters of datapoints')
@@ -137,7 +139,54 @@ plt.xlabel('Number of Clusters')
 plt.ylabel('WCSS')
 plt.show()
 
+-------------------------------------------------------------------------------------------
+"""
+2) Perform K-means clustering again to further distinguish speeding drivers 
+from those who follow speed limits, in addition to the rural vs. urban division.
+Label accordingly for the 4 groups.
+"""
+ 
 
+from sklearn.cluster import KMeans
+
+kmeans = KMeans(n_clusters = 4, init = 'k-means++', random_state = 0)
+
+features = dataset.iloc[:, [1,2]].values
+
+pred_cluster = kmeans.fit_predict(features) 
+
+print(pred_cluster) # Its the cluster id with 0,1,2,3 
+
+print(features[pred_cluster == 0])
+
+print(features[pred_cluster == 1])
+
+print(features[pred_cluster == 2])
+
+print(features[pred_cluster == 3])
+
+
+# Visualising the clusters
+plt.scatter(features[pred_cluster == 0, 0], features[pred_cluster == 0, 1], c = 'blue', label = 'Follow speed in Rural')
+plt.scatter(features[pred_cluster == 1, 0], features[pred_cluster == 1, 1], c = 'red', label = 'Not follow speed in Urban')
+plt.scatter(features[pred_cluster == 2, 0], features[pred_cluster == 2, 1], c = 'green', label = 'Follow speed in Urban')
+plt.scatter(features[pred_cluster == 3, 0], features[pred_cluster == 3, 1], c = 'olive', label = 'Not follow speed in Rural')
+
+
+print(kmeans.cluster_centers_)  # There are two points for 2 centroid   Rural follow speed
+
+print(kmeans.cluster_centers_[:, 0]) 
+print(kmeans.cluster_centers_[:, 1])
+# Central Location of the Cluster == Centroid
+
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c = 'yellow', label = 'Centroids')
+
+
+plt.title('Clusters of datapoints')
+plt.xlabel('Distance')
+plt.ylabel('Speed')
+plt.legend()
+plt.show()
 
 
 
