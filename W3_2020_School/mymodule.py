1b480d1aef990b9187735d0496628d40b1568f1a
Rajesh-sharma92 <sharma90126@gmail.com>
2020-04-23 19:32:20 +0530

    Python_Modules_created_file_mymodule.py

---
+++ W3_2020_School/mymodule.py
@@ -0,0 +1,37 @@
+
+'''
+NOTE :- Here we have created some of the Functions and we can use it whenever we need it.
+'''
+
+def greeting(name):
+    print('Hello and Welcome to the , ' + name)
+
+
+# Addition 
+def add(x,y):
+    print('The Addition x & y are :', (x+y))
+    
+# sub   
+def sub(x,y):
+    print('The Sub x & y are :', (x-y))
+# Mul
+def mul(x,y):
+    print('The Mul x & y are :', (x*y))
+# Div
+def div(x,y):
+    print('The Div x & y are :', (x/y))
+
+
+# To create the Dictionary type Variables.
+
+person = {
+  "name": "RAJESH USHA BABU",
+  "age": 26,
+  "country": "Sikar,Rajastha,India"
+}
+
+# To create the Data Into List.
+
+list1 = [2020,'Rajesh-Usha-Babudaya',20,40,50]
+
+
