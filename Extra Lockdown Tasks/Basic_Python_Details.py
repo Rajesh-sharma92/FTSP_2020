db6a29dd57dcc5409b6a7e049405327a78090ffc
Rajesh-sharma92 <sharma90126@gmail.com>
2020-04-30 17:28:16 +0530

    Python_Introduction

---
+++ Extra Lockdown Tasks/Basic_Python_Details.py
@@ -0,0 +1,125 @@
+
+       **********      Python Introduction   *************
+   ------------------------------------------------------------
+   
+
+What is the python ?
+
+1) It is the general purpsose programming languages.
+2) It is very popular now a days.
+3) It can be used most of the times in the server side to create 
+   the Web Apps.
+
+Syllabus :- 
+------------
+
+Syntax
+
+Comments Line 
+
+How to take Inputs from User ?
+ 
+Variables
+
+Data Types -- Int , Float , Strings, Complexs , Booleans.
+
+Data Structures like List , Tuples , Dictionary and Sets.
+
+If else Conditions
+
+For loops and While Loops
+
+Functions 
+
+------------------------------------------------------------------------
+
+EX :- 
+
+x = 10
+
+y = 20
+
+
+print('The Sum of x & y are :', (x+y))
+print('The Sub of x & y are :', (x-y))
+print('The Mul of x & y are :', (x*y))
+print('The Div of x & y are :', (x/y))
+
+print('Tech Guys Coding Studio')
+
+print("Tech Guys Coding Studio")
+
+'''
+Comments Line :- 
+-------------
+'''
+1) Single Line Comment.
+
+# print('This is Basic Python Class')
+
+print('This is Advanced Python Class')
+
+x = 100
+#y = 200
+print(x)
+print(y)
+
+
+2) Multi Line Comments :- 
+
+#x = 100
+#y = 230
+#print('This is Python Codes')
+
+'''
+x = 100
+y = 230
+print('This is Python Codes')
+'''
+
+How to take Inputs from User ? 
+------------------------------
+
+x = input('Enter the x value :')
+
+x = int(input('Enter the x value :'))
+print(x)
+
+Indentation :- 
+----------------
+x = 100
+y = 200
+
+if x < y:
+    
+    print('This is correct')
+    
+Ex :- 
+    
+x = 300
+y = 200
+
+if x < y:
+    print('This is correct')
+else:
+    print('This is not Correct')
+        
+    
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
