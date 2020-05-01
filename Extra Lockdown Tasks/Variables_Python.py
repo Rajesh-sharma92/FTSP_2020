0429bc9e4bc798a2445b3f3798a3d2617078c6c7
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-01 21:01:03 +0530

    Python_Variables

---
+++ Extra Lockdown Tasks/Variables_Python.py
@@ -0,0 +1,56 @@
+'''
+Variables are in Python :- 
+--------------------------
+'''
+x = 100
+print(x)
+print(type(x))
+
+y = 'Rahul'
+print(type(y))
+
+# Multiple values for multi variables.
+x , y , z = 10 , 20 , 'Apple'
+print(x)
+print(y)
+print(z)
+print(type(z))
+
+# One value to multiple Variables.
+
+x = y = z = 'Apple'
+print(x)
+print(y)
+print(z)
+
+
+x = 'Apple'
+
+y = ' Banana'
+
+z = x+y 
+print(z)
+
+# Note :- Always Aplhabetic or underscore.
+x = 100
+
+_ = 100
+
+print(_)
+print(type(_))
+
+x_ = 200
+
+2target = 100
+
+# Variables are Case Sensitive.
+
+age = 100
+print(age)
+
+Age = 100
+
+AGE = 100
+
+Python Tutorial For Beginners | Global Keyword in Python | Global vs Local Variable
+
