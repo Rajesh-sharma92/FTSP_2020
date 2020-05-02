813c4c0058920915e7d903d3d60f8355ef6f22e1
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-02 20:23:58 +0530

    Data_Types_Python

---
+++ Extra Lockdown Tasks/Data_Types_Python.py
@@ -0,0 +1,64 @@
+'''
+Data Types in Python :- 
+----------------------
+'''
+1) None
+2) Numeric - int , float , complex , bool
+3) List
+4) Tuple
+5) set
+6) string
+7) Range
+8) Dictionary
+
+1) None :- 
+------------
+x = 100
+print(x)
+print(type(x))
+
+x = None
+print(x)
+print(type(x))
+
+a = 10
+print(a)
+print(type(a))
+
+b = 20.5 
+print(type(b))
+
+compelx :- a + ib
+
+c = complex(a,b)
+print(c)
+print(type(c))
+
+bool :- 
+
+True , False , 1 , 0
+
+a = 100
+b = 20
+
+a > b 
+
+a < b
+
+a = 10
+print(type(a))
+
+b = float(a)
+print(b)
+print(type(b))
+
+
+
+
+
+
+
+
+
+
+
