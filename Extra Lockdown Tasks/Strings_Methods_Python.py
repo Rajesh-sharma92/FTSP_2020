1daa4f45613327ce5a98f799755b42df2e980b38
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-02 20:34:16 +0530

    Python_strings_Methods1

---
+++ Extra Lockdown Tasks/Strings_Methods_Python.py
@@ -0,0 +1,31 @@
+'''
+Strings Methods in Python :- 
+---------------------------
+'''
+str1 = 'PYTHON IS VERY SIMPLE'
+str2 = str1.lower()
+print(str2)
+
+print(str1)
+
+str1 = 'python is simple'
+str2 = str1.upper()
+print(str2)
+
+str1 = 'Python is very easy'
+str2 = str1.replace('easy','Very easy')
+
+print(str2)
+
+str1 = 'Python is simple simple and simple'
+str2 = str1.count('simple')
+print(str2)
+
+str2 = str1.find('simple')
+print(str2)
+
+str2 = str1.index('simple')
+print(str2)
+
+
+
