c71603d5dd75b5f410545a00d871a4926ca0f783
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-02 20:33:11 +0530

    Python_strings_Methods

---
+++ Extra Lockdown Tasks/Python_Strings_Methods.py
@@ -0,0 +1,42 @@
+'''
+String Data Types in Python :-
+------------------------------
+'''
+x = 'Python Easy'
+x = "Python Easy"
+x = '''Python Easy'''
+
+print(x)
+
+print(type(x))
+
+# in and not in 
+y = 'Python Easy is simple'
+
+x = 'one' in y
+print(x)
+print(type(x))
+
+# NOTE :- Length of string.
+
+x = 'Python simple'
+print(len(x))
+
+'''
+Slicing in Python :- 
+--------------------
+'''
+     012345 678910
+x = 'Python simple'
+
+x[2:10:2]
+
+x[-6:10:2] 
+
+x = 'python'
+y = 'p'
+print(type(x))
+print(type(y))
+
+
+
