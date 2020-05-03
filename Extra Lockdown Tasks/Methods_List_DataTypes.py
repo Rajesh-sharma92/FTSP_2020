30cd95414ac44887705ff74ad35cd1051fb75ec6
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-03 20:39:27 +0530

    Python_List_DataTypes_Methods

---
+++ Extra Lockdown Tasks/Methods_List_DataTypes.py
@@ -0,0 +1,44 @@
+'''
+Methods of List Data Types:- 
+----------------------------
+'''
+1) Append() :- 
+-----------
+
+l1 = [10,20,30,40,50]
+l1.append('Python')
+print(l1)
+
+l1.append(100)
+
+2) pop():- 
+--------
+l1.pop(0)
+
+3) Insert() :- 
+
+l1 = [10,20,30]
+l1.insert(0,'Python')
+print(l1)
+
+l1.index(30)
+
+l1.remove('Python')
+print(l1)
+
+l1 = [10,20,30,40,10,20,10]
+l1.count(10)
+
+l1.clear()
+print(l1)
+
+l1 = ['Apple',10,101,'Book']
+l1.reverse()
+l1
+print(l2)
+
+l1 = [101,200,10,24,55]
+l1.sort()
+l1
+
+
