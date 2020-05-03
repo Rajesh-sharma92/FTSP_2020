01a9bc61c702899fc92ac848b9e377eb91a7e2b9
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-03 20:38:03 +0530

    Python_List_DataTypes

---
+++ Extra Lockdown Tasks/ListData_Types_Python.py
@@ -0,0 +1,53 @@
+'''
+List Data Types in Python :- 
+----------------------------
+'''
+NOTE :- list is Data Types and it is 
+used to stored the elements.
+
+Ex :-
+
+list1 = [] # Empty list
+
+list1 = [10,20,30,40,50]
+print(list1)
+print(type(list1))
+
+# NOTE : - Slicing we can do it into lists.
+
+list1[2]
+
+list1[-1]
+
+list1[1:3]
+
+list2 = [10,20,30,10,40,20]
+
+print(list2)
+
+list2[2:4]
+
+Hetrogenius data types.
+
+list1 = [10,20,'Rahul',-101,0,'star']
+list1
+
+list1 = [10,20,'rahul']
+print(len(list1))
+
+list1.append('gandhi')
+
+print(list1)
+list1
+print(len(list1))
+
+l1 = [10,20,30]
+l2 = ['raj',55]
+
+l3 = l1+l2
+print(l3)
+
+
+
+
+
