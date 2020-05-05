89d921f7dc3ff8357d23912a43553687b0f96231
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-05 19:55:21 +0530

    Number_System_Python

---
+++ Extra Lockdown Tasks/Number_System_Conversion.py
@@ -0,0 +1,56 @@
+# Number System Conversion :- 
+
+1) Binary 
+2) Decimal
+3) Octal
+4) Hexadecimal
+
+1) Binary :- 
+---------------
+We know base 2 , 0 & 1.
+
+2) Decimal :- 
+-----------
+we know that base 10 , 0 -9 .
+
+3) Octal :- 
+-----------
+Base - 8 , 0-7.
+
+4) Hexadecimal :-  
+--------------
+Base - 16 and 0 -9 & A-F
+
+Ex :- 
+
+25 # decimal
+x = bin(25)
+print(x)
+print(type(x))
+
+y = 0b11001
+print(y)
+
+# decimal to octal .
+
+x = oct(25)
+print(x)
+
+# Octal to decimal.
+
+y = 0o31
+print(y)
+
+z = 0O31
+print(z)
+
+x = hex(25)
+print(x)
+
+y = 0x19
+print(y)
+
+y = 0X55
+print(y)
+
+
