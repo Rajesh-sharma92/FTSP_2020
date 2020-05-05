0afef1a6f768a0fcd785c0efb3dd8bd2d8b719b0
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-05 19:57:13 +0530

    Operators_In_Python

---
+++ Extra Lockdown Tasks/Operators_Python.py
@@ -0,0 +1,77 @@
+# Operators in Python :- 
+
+1) Arithematic
+2) Assignement
+3) Comparison
+4) Logical 
+5) Membership 
+6) Identity 
+7) Bitwise 
+
+Ex :- 
+
+x = 2
+y = 3
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**y)
+
+Ex :- 
+
+x = 1001
+print(x)
+
+x+=9
+print(x)
+
+x-=9
+print(x)
+
+x*=9
+print(x)
+
+Ex :- 
+==
+>
+<
+>= , <= , !=
+
+x = 100 
+y = 500
+
+print(x==y)
+
+print(x<y)
+
+print(y>x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
