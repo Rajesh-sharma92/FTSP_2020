1487ac8caf759ad159ae3418f972748fb159c07a
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-07 17:28:23 +0530

    Import_Math_Module

---
+++ Extra Lockdown Tasks/Import_Math_Module.py
@@ -0,0 +1,56 @@
+# How to Import Module
+
+# Math Module
+
+x = sqrt(9)
+print(x)
+
+import math
+
+dir(math)
+
+print(math.sqrt(9))
+print(math.sqrt(16))
+
+floor , ceil
+
+2.5 = 3 
+2.1 = 2
+
+print(math.floor(2.9))
+print(math.ceil(2.5))
+
+print(math.ceil(2.1))
+
+
+print(2**3)
+print(math.pow(2,3))
+
+pi = 3.14
+print(math.pi)
+
+print(math.e)
+
+print(math.sin(90))
+print(math.tan(45))
+
+# Alising for the method
+import math as m 
+
+print(math.sqrt(25))
+print(m.sqrt(25))
+
+dir(math)
+
+from math import sqrt
+print(sqrt(4))
+
+from math import pow
+print(pow(2,3))
+
+help('math')
+
+
+print(help('math'))
+
+
