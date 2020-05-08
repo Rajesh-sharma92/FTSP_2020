dc103fc75634049c9e00fdd6445050500cd85df4
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-08 20:47:41 +0530

    Break_Continue_Pass_Statements

---
+++ Extra Lockdown Tasks/Break_Continue_Pass_Python.py
@@ -0,0 +1,97 @@
+# Break , Continue & Pass :- 
+
+Ex :- 
+
+i = 1
+while i < 5:
+    if i==3:
+        print('Three got it')
+        break
+    print('iphone')
+    i = i + 1
+    # i+= 1
+    
+for i in range(5):
+    if i==3:
+        print('Yes')
+        break
+    print(i)
+    
+# Continue :-
+
+for i in range(10):
+    if i==5:
+        print('Not there 5')
+        continue # Current value
+    print(i)
+    
+    
+#paSS :- 
+
+for i in range(10):
+    if i%2!=0:
+        pass
+    else:
+        print(i)
+
+Ex :- 
+
+if 10>5:
+    print('Yes')
+
+if True:
+    pass
+
+    
+    
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
