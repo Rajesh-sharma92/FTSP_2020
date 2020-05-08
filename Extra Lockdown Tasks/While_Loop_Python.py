6b6d700c8f900848867c7d0a813f96f47593edf5
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-08 11:20:45 +0530

    While_Loop_Python

---
+++ Extra Lockdown Tasks/While_Loop_Python.py
@@ -0,0 +1,46 @@
+# While loop in Python 
+
+print('Python')
+print('Python')
+print('Python')
+print('Python')
+
+Ex :- 
+
+i = 1
+while i<=5:
+    print('Python')
+    i = i + 1
+    
+Ex :- 
+
+
+i = 5
+while i>=1:
+    # print('Python' , i)
+    print(i)
+    i = i - 1
+    
+# Nested While Loop :- 
+
+Python Coding Coding Coding
+Python Coding Coding Coding
+Python Coding Coding Coding
+Python Coding Coding Coding
+Python Coding Coding Coding
+
+i = 1
+while i <=5: # Outer While Loop
+    print('Python' , end=' ')
+    
+    j = 1
+    while j<=3: # Inner While Loop
+        print('Coding' , end=' ')
+        j = j + 1
+    
+    i = i + 1
+    print()
+    
+
+    
+    
