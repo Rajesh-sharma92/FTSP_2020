7e6b36d1801e1a90380ec23eea841abff0abc6f4
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-08 20:45:46 +0530

    If_Else_Elif_Python

---
+++ Extra Lockdown Tasks/If_Elif_Else_Python.py
@@ -0,0 +1,63 @@
+# if , elif & else :- 
+Ex :- 
+
+if True:
+    print('Yes!! do it')
+
+print('Hello world')    
+    
+Ex :- 
+if False:
+    print('No leave it')
+
+print('Python Coding')
+
+Ex :- 
+if False:
+    print('No leave it')
+
+    print('Python Coding')
+
+Ex :- 
+x = 200
+y = 1000
+if x > y:
+    print('Yes')
+else:
+    print('No')
+
+Ex :- 
+
+x = 100
+y = 10
+
+if (y<x):
+    print('Working')
+else:
+    print('Not working')
+
+Ex :- Elif , if ,else
+
+ten - 10 
+twenty - 20
+thirty - 30
+fourty - 40
+
+x = 40 
+
+if x==10:
+    print('Ten')
+
+elif x==20:
+    print('Twenty')
+
+elif x==30:
+    print('Thirty')
+    
+elif x==40:
+    print('Fourty')
+    
+else:
+    print('Not Matching No.')
+
+
