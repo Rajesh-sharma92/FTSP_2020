ed0151cf3077cfbb6b396e98ef21ebab1bba18bf
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-08 20:48:52 +0530

    Break_Continue_Pass_Verses

---
+++ Extra Lockdown Tasks/Break_Continue_Pass_Verses.py
@@ -0,0 +1,36 @@
+# Break Vs. Continue Vs. Pass :- 
+ Ex :- 
+ 
+for i in range(5):# 0,1,2,3,4
+    if i==3:
+        continue # rkip Cut
+    
+    print('Python' ,i)
+
+Python 0
+Python 1
+Python 2
+Python 3
+Python 4 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
