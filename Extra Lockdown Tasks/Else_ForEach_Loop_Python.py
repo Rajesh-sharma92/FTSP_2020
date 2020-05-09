955e59834e8aa83e96182a92083b990761347dc6
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-09 17:42:12 +0530

    ForEachLoop_with_Else_Python

---
+++ Extra Lockdown Tasks/Else_ForEach_Loop_Python.py
@@ -0,0 +1,49 @@
+# else condition for each loop
+
+Ex :- 
+x = 100
+y = 20
+
+if x>y:
+    print('Yes')
+else:
+    print('No')
+
+# for each loop & else block
+Ex :- # divisble by 5.
+
+l1 = [11,13,15,14,21,25]
+
+for x in l1:
+    if x%5==0:
+        print(x)
+        break
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
