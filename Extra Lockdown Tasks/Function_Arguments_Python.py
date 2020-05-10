ce6ce176d6012dc2a108e571a0eb7bb0c75ee5b5
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-10 20:12:25 +0530

    Function_Arguments_Python

---
+++ Extra Lockdown Tasks/Function_Arguments_Python.py
@@ -0,0 +1,49 @@
+# Function Arguments in Python :-
+
+Ex:- 
+
+def f1(x): # Define
+    print('x Value :', x)
+    
+f1(2020) # Calling    
+f1(2030)
+
+Ex :- 
+def add(x,y):
+    print('The Addition :',(x+y))
+    
+add(10,20)
+add(2020,10)
+
+Ex :- 
+def add(x,y,z):
+    print('The Addition :',(x+y+z))
+    
+add(10,20,30)
+add(2020,10,20)
+
+Ex :- 
+
+def s1(x):
+    print('The String :', x)
+    
+s1('Python Coding Studio')    
+    
+Ex :- 
+
+
+def list1(x):
+    print('The List :', x)
+    
+list1([10,20,30])    
+
+Ex :- 
+
+def f1(x):
+    x = 100 # Reassigning the value
+    print('X value :', x)
+    
+f1(200)    
+
+
+
