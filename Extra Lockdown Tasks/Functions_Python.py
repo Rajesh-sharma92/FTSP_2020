949cd9620f38e21157b9b3ebdca9339515a24bdc
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-10 16:09:18 +0530

    How to create & Calling the Functions

---
+++ Extra Lockdown Tasks/Functions_Python.py
@@ -0,0 +1,41 @@
+# Functions in Python :- 
+1) It is very useful in coding.
+2) It is used for Code Reusablity.
+3) It is easy & understand also.
+4) It is gives the better result.
+5) It contains 2 parts .
+Like Creating/Defining & Calling.
+
+print('Hello world')
+
+Ex :- 
+
+def Test(): # Function Creating / Defining
+    print('Hello Friends!!!')
+    print('Gud Evening!!')
+    print(10*2)
+    
+Test() # Function calling.
+
+Ex :- 
+
+def add(x,y):
+    return (x+y)
+
+add(10,40)
+
+
+Ex :- 
+
+def sub_mul(x,y):
+    x1 = (x-y)
+    x2 = (x*y)
+    return x1,x2
+
+res1 , res2 = sub_mul(10,2) # Calling
+
+print(res1)
+print(res2)    
+    
+
+
