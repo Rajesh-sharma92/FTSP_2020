e70e21bbaa0cd30bd95cf9d1e010bfca15f0e287
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-12 16:23:03 +0530

    Factorial_Python

---
+++ Extra Lockdown Tasks/Factorial_Python.py
@@ -0,0 +1,40 @@
+# Factorial in Python :- 
+5! = 5x4x3x2x1 = 120
+4! = 24
+0! = 1
+
+Ex :- 
+def fact(n):
+    f = 1 # 1,2,6,24,120
+    for i in range(1,n+1):# 1,2,3,4,5,6
+        f = f*i
+    return f
+
+n = int(input('Enter n Value :'))
+fact(n)
+
+Ex :- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
