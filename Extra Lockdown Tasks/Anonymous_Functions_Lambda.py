939ec795ca19320adf30629d245727f40ded0c4a
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-13 12:46:06 +0530

    Anonymous Function OR Lambda Function in Python

---
+++ Extra Lockdown Tasks/Anonymous_Functions_Lambda.py
@@ -0,0 +1,36 @@
+# Anonymous Functions or Lambda :- 
+
+Ex :- 2 nos. addition
+
+def add(x,y):
+    return (x+y)
+
+res = add(10,20)
+print('The Result :', res)
+
+Ex :- square of a no.
+
+def square(n):
+    return n**2 # return n*n
+
+res = square(5)
+
+# Lambda Function :- print('The Result :', res)
+
+
+Syntax :- 
+
+lambda x : expression
+
+f = lambda x,y : x+y
+res = f(4,6)
+print('The Result :', res)
+
+Ex :- 
+
+f = lambda x : x*x
+res = f(6)
+print('The Result :', res)
+
+
+
