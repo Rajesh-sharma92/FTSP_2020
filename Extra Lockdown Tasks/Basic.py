e3e0935f9eba421af5d1c5973e02de7fabb592ac
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-14 16:12:47 +0530

    Basic_Modules

---
+++ Extra Lockdown Tasks/Basic.py
@@ -0,0 +1,35 @@
+# How to create the Own Module in Python :- 
+
+# saves with extension with ".py"
+
+def greetings(name):
+    print('Hello & Welcome to the ' + name)
+
+def add(x,y):
+    print('The Addition :', (x+y))
+    
+def sub(x,y):
+    print('The Sub :', (x-y))
+
+def mul(x,y):
+    print('The Mul :', (x*y))
+
+def div(x,y):
+    print('The Div :', (x/y))    
+    
+def square(x):
+    print('The square :', (x*x))
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
