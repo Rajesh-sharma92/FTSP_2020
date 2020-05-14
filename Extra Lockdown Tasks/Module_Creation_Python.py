f0c1b3dcbd68a22d35b8774ae403fd216dcde85d
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-14 16:14:48 +0530

    How to call the Module & its Functions in Python

---
+++ Extra Lockdown Tasks/Module_Creation_Python.py
@@ -0,0 +1,50 @@
+import math
+
+dir(math)
+
+print(math.sqrt(16))
+
+
+import Basic
+
+Basic.greetings('Python Coding Studio')
+Basic.greetings('India')
+
+Basic.add(10,20)
+Basic.sub(20,10)
+Basic.mul(2,5)
+Basic.div(20,5)
+
+# Another Name for the Modules or Alaising
+import Basic as bs
+bs.greetings('India Gate')
+
+bs.add(20,50)
+
+# To Import the specify function only.
+from Basic import div
+div(30,5)
+
+Basic.square(4)
+
+dir(Basic)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
