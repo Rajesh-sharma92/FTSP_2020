2aff87ecab3b7b7250622c0dae62d3f747dd50e1
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-20 16:06:48 +0530

    What is Class,Object,ReferenceVariables in Python

---
+++ Extra Lockdown Tasks/Advanced_Python/Advanced_Python_Introduction.py
@@ -0,0 +1,66 @@
+        !!! ADVANCED PYTHON CONCEPTS !!!
+
+1) What are Objects in Python ?
+
+2) What are Classes in Python ?
+
+3) What are Reference Variables in Python ?
+
+4) What are Self Variable in Python ?
+
+5) Types of Variables in Python ?
+    i) Instance  ii) Class level  
+    iii) Static  iv) Local
+
+6) Types of Method in Python ?
+i) Instance Method ii) Static Method iii) Class Method
+
+7) What is Inner Class in Python ?
+
+8) What is Garbage Collection in Python ?
+
+9) Polymorphism ( Operator Overloading ) in Python ?
+
+10) Method Overloading & Method Overriding in Python ?
+
+11) Types of Inheritance in Python ?
+    i) Single ii) Multiple 
+    iii) Multilevel iv) Hybrid v) cyclic
+ 
+12) What is Super() method in Python ?
+
+13) File Handling in Python ?
+
+14) Multithreading in Python ?
+
+15) Generators in Python ?
+
+16) Decorators in Python ?
+
+17) Iterators in Python ?
+
+18) Abstraction 
+
+19) Encapsulation 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
