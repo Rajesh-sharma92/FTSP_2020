818f4bc1e27bbca2d46d4b75dc56e019834ceb82
Rajesh-sharma92 <sharma90126@gmail.com>
2020-05-22 20:52:29 +0530

    How to Filtering the Rows by Column Value

---
+++ Ritchie Ng Pandas/Filtering_rows_pandas_Column_Value.py
@@ -0,0 +1,149 @@
+# Filtering rows of a pandas DataFrame by column value :-
+
+import pandas as pd
+
+url = 'http://bit.ly/imdbratings'
+
+df = pd.read_csv(url)
+
+df.head()
+
+df.columns
+
+df.shape # (979, 6)
+
+# booleans
+print(type(True)) # bool
+print(type(False)) # bool
+
+Q.1. We want to create a list of booleans with the same number of 
+     rows as the movies' DataFrame
+
+ True if duration > 200
+ False if otherwise
+
+# How to create Empty list.
+booleans = [] # Empty list
+
+# Apply the for each loop now.
+ 
+for length in df['duration']:
+    if length >= 200:
+        booleans.append(True)
+    else:
+        booleans.append(False)
+        
+booleans[7:12]    
+------------
+[True, False, False, False, False]
+
+# To check the length of Booleans.
+print(len(booleans)) # 979
+
+
+# To convert booleans into a Pandas series.
+
+is_long = pd.Series(booleans)
+
+is_long.head()
+-------------------
+0    False
+1    False
+2     True
+3    False
+4    False
+dtype: bool
+
+df.columns
+---------------
+Index(['star_rating', 'title', 'content_rating', 'genre', 
+       'duration',  'actors_list'] , dtype='object')
+
+df['genre'].head()
+--------------------
+0     Crime
+1     Crime
+2     Crime
+3    Action
+4     Crime
+Name: genre, dtype: object
+
+
+# this pulls out duration >= 200mins.
+
+df[is_long].head() # It will work
+
+
+
+ '''  Faster method without a for loop  '''
+
+# this line of code replaces the for loop
+# when you use a series name using pandas and use a comparison 
+# operator, it will loop through each row.
+ 
+is_long = movies['duration'] >= 200
+is_long.head()
+-----------
+0    False
+1    False
+2     True
+3    False
+4    False
+Name: duration, dtype: bool
+
+
+df[is_long].head()
+
+
+''' Even better way to simplify movies[is_long] '''
+
+
+df[ df['duration'] >= 200 ].head() # it will working.
+
+
+
+
+# Additional tip: we want to study the duration and only 
+# the genre instead of all the columns
+
+# this is a DataFrame, we use dot or bracket notation to get what we want
+df[ df['duration'] >= 200]['genre'].head(10)
+--------------------
+2          Crime
+7      Adventure
+17         Drama
+78         Crime
+85     Adventure
+142    Adventure
+157        Drama
+204    Adventure
+445    Adventure
+476        Drama
+Name: genre, dtype: object
+
+
+
+# NOTE :-  best practice is to use .loc instead of what
+#      we did above by selecting columns.
+
+df.loc[ df['duration'] >= 200 , 'genre'].head(10)
+----------------
+2          Crime
+7      Adventure
+17         Drama
+78         Crime
+85     Adventure
+142    Adventure
+157        Drama
+204    Adventure
+445    Adventure
+476        Drama
+Name: genre, dtype: object
+
+
+ 
+ 
+
+
+
+
