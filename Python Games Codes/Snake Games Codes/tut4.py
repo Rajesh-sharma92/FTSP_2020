5cffeaf6a92d02d9e47426bc537e00d1df310dab
Rajesh-sharma92 <sharma90126@gmail.com>
2020-06-28 20:56:57 +0530

    Snake Games Series Code

---
+++ Python Games Codes/Snake Games Codes/tut4.py
@@ -0,0 +1,49 @@
+import pygame
+x = pygame.init()
+# print(x)
+
+# creating Window
+gameWindow = pygame.display.set_mode((1200,500))
+pygame.display.set_caption("My First Game")
+
+# Game Specific Variables
+exit_game = False
+game_over = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
