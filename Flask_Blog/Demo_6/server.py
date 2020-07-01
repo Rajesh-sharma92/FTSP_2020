fb5ce92214b8d604ffa5f94e9d68e7426037316d
Rajesh-sharma92 <sharma90126@gmail.com>
2020-07-01 22:19:06 +0530

    Flask Series Code

--- Flask_Blog/Demo_6/server.py
+++ Flask_Blog/Demo_6/server.py
@@ -11,6 +11,13 @@ def home():
 def about():
 	return render_template("about.html")
 
+
+@app.route("/Images")
+def images():
+	return render_template("images.html")
+
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
 
