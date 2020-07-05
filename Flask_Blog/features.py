041122aa128aa7163ddafbfb9b671efaf4f840c6
Rajesh-sharma92 <sharma90126@gmail.com>
2020-07-05 22:19:59 +0530

    Flask series code

--- Flask_Blog/features.py
+++ Flask_Blog/features.py
@@ -1,5 +1,7 @@
 from flask import Flask , render_template
+
 app = Flask('__name__')
+
 
 posts = [
 {
@@ -20,15 +22,18 @@ posts = [
 
 ]
 
+
 @app.route('/')
 @app.route('/home')
 def home():
 	return render_template('home.html' , posts=posts)
 
 
+
 @app.route('/about')
 def about():
 	return render_template('about.html' , title='about')
+
 
 
 if __name__ == '__main__':
