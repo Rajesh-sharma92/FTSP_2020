cb5e1306af8174f45e4bd8410eedc0e359a3a228
Rajesh-sharma92 <sharma90126@gmail.com>
2020-07-06 20:57:20 +0530

    Flask series code

--- Flask_Blog/Flask_Tutorial/Tut2.py
+++ Flask_Blog/Flask_Tutorial/Tut2.py
@@ -1,20 +1,26 @@
+
 from flask import Flask , render_template
 
 
+
 app = Flask(__name__)
+
 
 
 @app.route("/index")
 def hello():
     return render_template("index.html")
 
+
 @app.route("/bstp")
 def boot():
     return render_template("bootstrap.html")
 
+
 @app.route("/bootstrap")
 def bootstrap():
     return render_template("bootstrap1.html")    
+
 
 
 app.run(debug=True)
